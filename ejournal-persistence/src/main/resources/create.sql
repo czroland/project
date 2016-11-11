d203542e45d0df7f7420655c53e5f4f987d0d772
Czégényi Roland <czegenyi.roland@gmail.com>
2016-11-11 01:33:23 +0100

    Update "terms of use"

--- ejournal-persistence/src/main/resources/create.sql
+++ ejournal-persistence/src/main/resources/create.sql
@@ -1,4 +1,4 @@
-INSERT INTO public.users( id, name, email, password ) VALUES (1, 'teacher','123','123');
+INSERT INTO public.users( id, name, firstname, lastname, email, password ) VALUES (-1, 'teacher','a','a','asd@gh.hu','$2a$10$N4rOzufDNCMGWZtyaNoM4ud.7CWRFtEIXirRlDKOf.hIauBeGErXq');
 INSERT INTO public.users( id, name, email, password ) VALUES (2, 'student','123','123');
 INSERT INTO public.teachers( id,user_id ) VALUES (1,2);
 INSERT INTO public.classes( id, year, teacher_id ) VALUES (1, '2010',1);
