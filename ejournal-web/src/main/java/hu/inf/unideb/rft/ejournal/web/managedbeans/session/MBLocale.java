07f58b315715141b0d7700d620bee35fcb3d5f25
Czégényi Roland <czegenyi.roland@gmail.com>
2016-12-08 21:07:40 +0100

    Creating package for session scoped beans

--- ejournal-web/src/main/java/hu/inf/unideb/rft/ejournal/web/managedbeans/session/MBLocale.java
+++ ejournal-web/src/main/java/hu/inf/unideb/rft/ejournal/web/managedbeans/session/MBLocale.java
@@ -1,5 +1,4 @@
-package hu.inf.unideb.rft.ejournal.web.managedbeans.request;
-
+package hu.inf.unideb.rft.ejournal.web.managedbeans.session;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -12,12 +11,12 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.Locale;
 
-
-
-@ManagedBean(name="localeBean")
+@ManagedBean(name = "localeBean")
 @SessionScoped
-public class MBLocale implements Serializable{
+public class MBLocale implements Serializable {
 
+    private static final long serialVersionUID = 1358179251745548546L;
+    
     private Locale locale;
 
     @PostConstruct
@@ -31,13 +30,13 @@ public class MBLocale implements Serializable{
 
     public void setLanguageToEnglish(ActionEvent event) throws IOException {
         locale = new Locale("en");
-        ExternalContext ec= FacesContext.getCurrentInstance().getExternalContext();
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
         ec.redirect(((HttpServletRequest) ec.getRequest()).getRequestURI());
     }
 
     public void setLanguageToHungarian(ActionEvent event) throws IOException {
         locale = new Locale("hu_hu");
-        ExternalContext ec= FacesContext.getCurrentInstance().getExternalContext();
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
         ec.redirect(((HttpServletRequest) ec.getRequest()).getRequestURI());
     }
 
